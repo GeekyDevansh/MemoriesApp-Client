21dbbb4bd6be7f9508a1548472342eef9323c9bd
Devansh <devanshkhullar11@gmail.com>
2022-01-29 15:13:45 +0530

    Minor Bug fixes

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const url = 'https://memoriesapp14.herokuapp.com/';
+const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);  //createPost requires your new post as a parameter 
